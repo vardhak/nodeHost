9b8f5e6189c2b8bbf1e36b4e8bf5ef252d9df1ec
vardhak <125583188+vardhak@users.noreply.github.com>
2023-07-27 14:51:28 +0530

    new file uploaded

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.end("hello world");
+})
+
+app.listen(port, hostname, () => {
+    console.log("listening.....");
+})
